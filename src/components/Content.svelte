18742f57bf607dbc22c39f6881da8041dfeee675
Andhika Setiawan <dhika.cf4@gmail.com>
2023-12-21 17:18:48 +0700

    First
    using tw-element library component

---
+++ src/components/Content.svelte
@@ -0,0 +1,181 @@
+<script>
+    import { onMount } from "svelte";
+
+    // Initialization for ES Users
+    import {
+    ScrollSpy,
+    initTE,
+    } from "tw-elements";
+
+    onMount(() =>{
+        initTE({ScrollSpy})
+    });
+</script>
+
+<!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com --> 
+<div class="grid grid-cols-5">
+    <div class="col-span-4">
+      <div
+        data-te-spy="scroll"
+        data-te-target="#scrollspy1"
+        data-te-offset="200"
+        class="relative h-1/3 overflow-auto">
+        <section id="example-1">
+          <h3 class="pb-3 pt-5 text-xl font-semibold">Section 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+        </section>
+        <section id="example-2">
+          <h3 class="pb-3 pt-5 text-xl font-semibold">Section 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+        </section>
+        <section id="example-3">
+          <h3 class="pb-3 pt-5 text-xl font-semibold">Section 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+          <section id="example-sub-A">
+            <h3 class="pb-3 pt-5 text-xl font-semibold">Subsection A</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Labore earum natus vel minima quod error maxime, molestias ut.
+              Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati
+              et reiciendis quaerat accusamus numquam.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Labore earum natus vel minima quod error maxime, molestias ut.
+              Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati
+              et reiciendis quaerat accusamus numquam.
+            </p>
+          </section>
+          <section id="example-sub-B">
+            <h3 class="pb-3 pt-5 text-xl font-semibold">Subsection B</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Labore earum natus vel minima quod error maxime, molestias ut.
+              Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati
+              et reiciendis quaerat accusamus numquam.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Labore earum natus vel minima quod error maxime, molestias ut.
+              Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati
+              et reiciendis quaerat accusamus numquam.
+            </p>
+          </section>
+        </section>
+        <section id="example-4">
+          <h3 class="pb-3 pt-5 text-xl font-semibold">Section 4</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            earum natus vel minima quod error maxime, molestias ut. Fuga
+            dignissimos nisi nemo necessitatibus quisquam obcaecati et
+            reiciendis quaerat accusamus numquam.
+          </p>
+        </section>
+      </div>
+    </div>
+  
+    <div>
+      <!-- Scrollspy -->
+      <div id="scrollspy1" class="sticky-top pl-3 text-sm" >
+        <ul data-te-nav-list-ref>
+          <li class="py-1">
+            <a
+              data-te-nav-link-ref
+              data-te-nav-link-active
+              class="bg-transparent px-[5px] text-neutral-600 shadow-none dark:text-neutral-200 active:bg-red-600"
+              href="#example-1"
+              >Section 1</a
+            >
+          </li>
+          <li class="py-1">
+            <a
+              data-te-nav-link-ref
+              class="bg-transparent px-[5px] text-neutral-600 shadow-none dark:text-neutral-200 active:bg-red-600"
+              href="#example-2"
+              >Section 2</a
+            >
+          </li>
+          <li class="py-1">
+            <a
+              data-te-nav-link-ref
+              class="bg-transparent px-[5px] text-neutral-600 shadow-none dark:text-neutral-200"
+              href="#example-3"
+              >Section 3</a
+            >
+            <ul data-te-nav-list-ref class="pl-3">
+              <li class="py-1">
+                <a
+                  data-te-nav-link-ref
+                  class="bg-transparent px-[5px] text-neutral-600 shadow-none dark:text-neutral-200"
+                  href="#example-sub-A"
+                  >Subsection A</a
+                >
+              </li>
+              <li>
+                <a
+                  data-te-nav-link-ref
+                  class="bg-transparent px-[5px] text-neutral-600 shadow-none dark:text-neutral-200"
+                  href="#example-sub-B"
+                  >Subsection B</a
+                >
+              </li>
+            </ul>
+          </li>
+          <li class="py-1">
+            <a
+              data-te-nav-link-ref
+              class="bg-transparent px-[5px] text-neutral-600 shadow-none dark:text-neutral-200"
+              href="#example-4"
+              >Section 4</a
+            >
+          </li>
+        </ul>
+      </div>
+      <!-- Scrollspy -->
+    </div>
+  </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,16 @@ const config = {
   content: [
     "./src/**/*.{html,js,svelte,ts,md}",
     "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
-
+  
   theme: {
     extend: {},
   },
 
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require("tw-elements/dist/plugin.cjs")
   ],
   darkMode: 'class',
   theme: {
